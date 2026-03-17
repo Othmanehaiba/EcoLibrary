aa14259e68d134de58e42356067e5250fcd95f1a
Othmanehaiba <othmanehaiba4@gmail.com>
2026-03-17 10:46:39 +0000

    i added the route for all the auths functions

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\AuthController;
+
 
 Route::get('/user', function (Request $request) {
     return $request->user();
@@ -11,4 +13,12 @@ Route::get('/test', function () {
     return response()->json([
         'message' => 'API library works!',
     ]);
+});
+
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::get('/me', [AuthController::class, 'me']);
+    Route::post('/logout', [AuthController::class, 'logout']);
 });
